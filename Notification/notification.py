a0a894b5a386898f64ff1ec223394a852e6598fd
Vihang Shah <83380615+svihang8@users.noreply.github.com>
2023-10-02 10:14:03 -0400

    Update notification.py
    security bug improved.

--- Notification/notification.py
+++ Notification/notification.py
@@ -3,12 +3,6 @@ import sys
 sys.path.append('.')
 from Database.Database import Database
 
-account_sid = 'AC9bd99fb335724f3ee5789ba55c1fa361'
-auth_token = '26ab824d1b179430eda3f227ec6cc534'
-twilio_phone = '+12075033620'
-
-
-
 def main(data = []):
     for data in data:
         name, body, phonenumber = data['name'], data['dishes'], data['phonenumber']
